eebd9d649b54bd96fe5999ab9c02f5ac88c2f14d
Daniil Gomza <89922067067@mail.ru>
2024-04-05 22:36:28 +0500

    created new project

---
+++ lesson-03/lesson-03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson-03
+//
+//  Created by Даниил Евгеньевич on 05.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
